f07bd55489ef91a59ef59af5adae6fc48478924f
Bre Thunholm <bthunholm@gmail.com>
2021-09-23 06:06:04 -1000

    implemented nprogress loader bar

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "8894c6b055648a445131";
+/******/ 	var hotCurrentHash = "932c8f691089ba8c1230";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
