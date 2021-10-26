20a8a4717dd3dd9e658c60424c00804246a1e810
Bre Thunholm <bthunholm@gmail.com>
2021-10-26 06:47:41 -1000

    added money formatter function and product styles

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "63a96bc67daed53fa50b";
+/******/ 	var hotCurrentHash = "0bb655f023421df6d41c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
